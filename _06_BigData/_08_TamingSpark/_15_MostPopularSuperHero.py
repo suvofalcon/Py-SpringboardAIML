fdc9335015bda86de13d7d20806899105b5e2e9b
suvofalcon <suvo.bhattacharya@gmail.com>
2021-06-03 19:22:17 +0530

    Most Obscure Heros

--- _06_BigData/_08_TamingSpark/_15_MostPopularSuperHero.py
+++ _06_BigData/_08_TamingSpark/_15_MostPopularSuperHero.py
@@ -4,15 +4,6 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as func
 from pyspark.sql.types import LongType, StringType, StructType, StructField, IntegerType
 
-'''
-Split off HeroID from the beginning of  the line
-Count how many space-separated numbers are in the line
-- Subtract 1 to get hold of total number of connections
-Group by HeroIDs to add up connections split into multiple lines
-Sort by total connections
-Filter name look up dataset by the most popular HeroID to look up the name
-'''
-
 # Create the spark session and initialize the app name
 spark = SparkSession.builder.appName("MostPopularSuperHero").getOrCreate()
 
@@ -25,6 +16,15 @@ names = spark.read.schema(schema=schema).option("sep"," ").csv("../resources/Mar
 
 # Load the entries data (without any schema)
 lines = spark.read.text("../resources/Marvel-Graph")
+
+'''
+Split off HeroID from the beginning of  the line
+Count how many space-separated numbers are in the line
+- Subtract 1 to get hold of total number of connections
+Group by HeroIDs to add up connections split into multiple lines
+Sort by total connections
+Filter name look up dataset by the most popular HeroID to look up the name
+'''
 
 # we need to trim each line of whitespaces as that could throw off the count
 connections = lines.withColumn("HeroID", func.split(func.trim(func.col("value")), " ")[0])\
