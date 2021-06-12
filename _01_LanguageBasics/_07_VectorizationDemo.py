2f5cd1f6a5cd6ce060de60e6c02a488668e12b0a
suvofalcon <suvo.bhattacharya@gmail.com>
2021-06-12 22:03:27 +0530

    Vectorization Demo in Python

---
+++ _01_LanguageBasics/_07_VectorizationDemo.py
@@ -0,0 +1,28 @@
+'''
+we will demonstrate how a vectorized version is faster than regular forloops
+'''
+
+import numpy as np
+import time
+
+# Two arbirary arrays
+a = np.random.rand(1000000)
+b = np.random.rand(1000000)
+
+# Performing vector product
+tic = time.time()
+vecProduct = np.dot(a, b)
+toc = time.time()
+
+print(f"Vector Product - {vecProduct}")
+print(f"Vector Product time - {1000 * (toc-tic)} ms")
+
+# Performing Normal For loop multiplication
+tic = time.time()
+normProduct = 0
+for index in range(1000000):
+    normProduct += a[index] * b[index]
+toc = time.time()
+
+print(f"For Loop Product - {normProduct}")
+print(f"For Loop Product time - {1000 * (toc-tic)} ms")
