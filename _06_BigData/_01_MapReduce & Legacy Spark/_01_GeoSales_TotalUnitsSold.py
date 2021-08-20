123e070d6f95165c0ffbc4509e4735a0fd29b4ab
suvofalcon <suvo.bhattacharya@gmail.com>
2021-08-20 23:27:56 +0530

    Mapreduce Python code using MRJob

--- _06_BigData/_01_MapReduce & Legacy Spark/_01_GeoSales_TotalUnitsSold.py
+++ _06_BigData/_01_MapReduce & Legacy Spark/_01_GeoSales_TotalUnitsSold.py
@@ -5,6 +5,7 @@ Geosales - Total units_sold by year for a given country and a given item type
 # Library imports
 from mrjob.job import MRJob
 from mrjob.step import MRStep
+from datetime import datetime
 
 # Define the main class which takes MRJob as an argument
 
@@ -18,11 +19,20 @@ class TotalUnitsSold(MRJob):
         ]
 
     # Mapper function
+    '''
+    This function, will extract the Year from order_date , country, item type and units sold
+    A composite key will be defined
+    '''
     def mapper_get_unitsSold(self, _, line):
         geosales = line.split(",")
-        yield (geosales[2], geosales[3]), int(geosales[9])
+        country, item_type, units_sold = geosales[2], geosales[3], int(geosales[9])
+        year = datetime.strptime(geosales[6], "%Y-%m-%d %H:%M:%S").year
+        yield (year, country, item_type), units_sold
     
     # Reducer function
+    '''
+    Reducer function will sum the units, sold by the composite key
+    '''
     def reducer_sum_unitsSold(self, key, value):
         yield key, sum(value)
 
