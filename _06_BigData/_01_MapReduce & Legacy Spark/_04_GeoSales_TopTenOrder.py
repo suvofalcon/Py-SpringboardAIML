828fc2c807f7b8a8d717cbb7112f465b400edc22
suvofalcon <suvo.bhattacharya@gmail.com>
2021-08-27 21:30:18 +0530

    MRJob top ten orders (WIP)

--- _06_BigData/_01_MapReduce & Legacy Spark/_04_GeoSales_TopTenOrder.py
+++ _06_BigData/_01_MapReduce & Legacy Spark/_04_GeoSales_TopTenOrder.py
@@ -3,10 +3,9 @@
 Geosales - Top 10 order id for a given year by the total_profit 
 
 """
-
-from typing import OrderedDict
 from mrjob.job import MRJob
 from mrjob.step import MRStep
+from heapq import nlargest
 
 MRJob.SORT_VALUES = True
 
@@ -14,8 +13,9 @@ class TopTenOrder(MRJob):
     def steps(self):
         return [
             MRStep(mapper=self.mapper_get_order_by_profit,
-                   reducer=self.reducer_arrange_by_profits),
-            MRStep(reducer=self.reducer_sorted_by_profits)
+                   combiner=self.combiner_arrange_by_profits,
+                   reducer=self.reducer_reduce_by_profits),
+            MRStep(reducer=self.reducer_sorted_by_topTen_profits)
         ]
 
 
@@ -31,16 +31,26 @@ class TopTenOrder(MRJob):
         yield (year, order_id), float(profit)
 
 
-    # Reducer Function
-    def reducer_arrange_by_profits(self, key, values):
+    # Combiner Function
+    def combiner_arrange_by_profits(self, key, values):
         # We are padding the values with 0 so that the string sorting happens correct
         yield str(values).zfill(5), key 
-        #yield key, sum(values)
-    
-
+        
     # Reducer Function
-    def reducer_sorted_by_profits(self, key, profits):
+    def reducer_reduce_by_profits(self, key, profits):
         yield key, profits
+    
+    def top_10(self, orderPair):
+        for orderTotal, itemID in orderPair:
+            top_orders = nlargest(10, orderTotal)
+        for top_orders in orderTotal:
+            return (orderTotal, itemID)
+    
+    # Reducer Function
+    def reducer_find_top10(self, key, orderPair):
+        orderTotal, country = self.top_10(orderPair)
+        yield orderTotal, country
+
            
 if __name__ == '__main__':
     TopTenOrder.run()
